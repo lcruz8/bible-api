04c23a5987dceb98f4487f47671e08f1dbf0fda0
Leilane Cruz <l_mcruz98@live.com>
2022-10-17 13:11:43 -0300

    Criação de rotas para TestamentoController

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/testamento/listar', [App\Http\Controllers\TestamentoController::class , 'index'])->name('testamento.index');
+Route::get('/testamento/{id}', [App\Http\Controllers\TestamentoController::class , 'show'])->name('testamento.detalhar');
+Route::post('/testamento/cadastrar', [App\Http\Controllers\TestamentoController::class , 'store'])->name('testamento.cadastrar');
+Route::put('/testamento/atualizar/{id}', [App\Http\Controllers\TestamentoController::class , 'update'])->name('testamento.atualizar');
+Route::delete('/testamento/apagar/{id}', [App\Http\Controllers\TestamentoController::class , 'destroy'])->name('testamento.apagar');
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
