52647c7a31534077609383ab3c8e2e1f659b610d
Leilane Cruz <l_mcruz98@live.com>
2022-10-17 13:11:25 -0300

    CRUD Testamento

---
+++ app/Http/Controllers/TestamentoController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Testamento;
+use Illuminate\Support\Facades\App;
+
+class TestamentoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Testamento::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $testamento = new Testamento();
+        $testamento->fill($request->all());
+        $testamento->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Testamento::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $testamento = Testamento::find($id);
+        $testamento->fill($request->all());
+        if($testamento->save()) 
+            App::abort(500, 'Erro ao salvar');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Testamento::destroy($id);
+    }
+}
