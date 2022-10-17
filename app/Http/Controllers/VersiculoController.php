3550e6986b2104c85778bdd04ed5002893130a5c
Leilane Cruz <l_mcruz98@live.com>
2022-10-17 14:43:07 -0300

    CRUD Versiculo

---
+++ app/Http/Controllers/VersiculoController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Versiculo;
+use Illuminate\Support\Facades\App;
+
+class VersiculoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Versiculo::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $versiculo = Versiculo::create($request->all());
+        $versiculo->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Versiculo::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $versiculo = Versiculo::find($id);
+        $versiculo->fill($request->all());
+        if(!$versiculo->save()) 
+            App::abort(500, 'Erro ao salvar');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Versiculo::destroy($id);
+    }
+}
