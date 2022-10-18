87a11560d1a58399f6bd0538ef399aad7c621808
Leilane Cruz <l_mcruz98@live.com>
2022-10-18 16:58:06 -0300

    Nova rota de registro autenticação

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\TestamentoController;
 use App\Http\Controllers\LivroController;
 use App\Http\Controllers\VersiculoController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -38,6 +39,8 @@ use Illuminate\Support\Facades\Route;
 // Route::apiResource('testamento', TestamentoController::class);
 // Route::apiResource('livro', LivroController::class);
 // Route::apiResource('versiculo', VersiculoController::class);
+
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::apiResources([
     'testamento' => TestamentoController::class,
