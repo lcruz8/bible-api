121f8d8c688894ef4ec313ace591990ff1855c3b
Leilane Cruz <l_mcruz98@live.com>
2022-10-19 11:37:06 -0300

    Implementação do login e logout auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -34,8 +34,30 @@ class AuthController extends Controller
 
     public function login(Request $request) {
         $fields = $request->validate([
-            'name' => 'required|string',
+            'email' => 'required|string',
             'password' => 'required|string',
         ]);
+
+        $user = User::where('email', $fields['email'])->first();
+
+        if(!$user || !Hash::check($fields['password'], $user->password)) {
+            return response(["message"=>"Credenciais inválidas"], 401);
+        } 
+
+        $token = $user->createToken('UsuarioLogado')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ],201);
+
+    }
+
+
+    public function logout(Request $request) {
+        auth()->user()->tokens()->delete();
+        return response([
+            "message" => "Deslogado com sucesso!"
+        ], 200);
     }
 }
